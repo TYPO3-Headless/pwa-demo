ad96443a5e1d86d06228d8211ffc14fe195bee81
Łukasz Uznański <l.uznanski@macopedia.com>
2022-07-29 16:02:13 +0200

    Fixes to package and repositroy

--- packages/site_package/Configuration/TCA/Overrides/sys_template.php
+++ packages/site_package/Configuration/TCA/Overrides/sys_template.php
@@ -2,7 +2,7 @@
 defined('TYPO3_MODE') || die();
 
 call_user_func(
-    function ($extensionKey) {
+    static function ($extensionKey) {
         /**
          * Default TypoScript for site_package
          */
--- packages/site_package/ext_emconf.php
+++ packages/site_package/ext_emconf.php
@@ -11,7 +11,7 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '9.5.0-10.4.99',
+            'typo3' => '11.5.0-11.5.99',
         ],
         'conflicts' => [],
         'suggests' => [],
