09bbab59ea84a0a32695ca25771631bcac24c3d2
Adam <a.marcinkowski@macopedia.com>
2023-03-07 21:25:39 +0100

    Handle page / initial data

---
+++ front/components/TheHeader.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="container mx-auto my-3">
+    <nav v-if="initial?.navigation?.length && initial.navigation[0].children">
+      <ul class="flex justify-center">
+        <li v-for="{link, title} in initial.navigation[0].children" :key="link" class="mx-1">
+        <T3NavLink :to="link">
+          {{ title }}
+        </T3NavLink>
+        </li>
+      </ul>
+    </nav>
+  </header>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      initial: ({typo3}) => typo3.initial
+    })
+  }
+}
+</script>
---
+++ front/pages/_.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <t3-dynamic
+      :data="t3page.content"
+      :type="t3page.appearance.backendLayout"
+      layout
+    />
+    <PageInformation/>
+  </div>
+</template>
+<script src="~typo3/pages/_.vue"/>
