cb8be17cb90dc408de832ca96f9a3dcfbe174708
Adam <a.marcinkowski@macopedia.com>
2023-03-07 20:02:04 +0100

    Kickstart application

---
+++ front/components/T3CeText.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <p><strong>Without parsing</strong></p>
+    {{ bodytext }}
+    <p><strong>with parsing</strong></p>
+    <T3HtmlParser :content="bodytext"/>
+  </div>
+</template>
+<script src="~typo3/components/T3CeText"/>
