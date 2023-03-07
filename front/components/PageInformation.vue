f93c24d83cb35b296a8d0db5de645efc55fc1b47
Adam <a.marcinkowski@macopedia.com>
2023-03-07 21:25:39 +0100

    Handle page / initial data

---
+++ front/components/PageInformation.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <p>I have all information about the page</p>
+    <pre>{{ page }}</pre>
+    <p>I have all information from initial data</p>
+    <pre> {{ initial }} </pre>
+  </div>
+</template>
+<script>
+import { mapState } from 'vuex'
+export default {
+  computed: {
+    ...mapState({
+      page: ({typo3}) => typo3.page,
+      initial: ({typo3}) => typo3.initial
+    })
+  }
+}
+</script>
