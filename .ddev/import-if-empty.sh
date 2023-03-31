0e758ec98a4dac6d077e49629d4987c90b938c20
Daniel Piętka <124677818+d-pietka@users.noreply.github.com>
2023-03-31 09:06:21 +0200

    Update TYPO3 to v12.3.0 (#81)
    * Update ddev config
    
    * Update db schema (can be removed once dump is updated to v12)
    
    * Mariadb 10.4 & set TYPO3 Context to development & migrate LocalConfiguration
    
    * Update TYPO3 to v12, add additional core extensions, update extensions
    
    * Update site package extension
    
    * Add missing folders in site package to avoid errors in BE
    
    * Use db as default cache backend
    
    * Updated database dump after migration to v12
    
    * Update headless extension
    
    * Update EXT:Headless
    
    * Tested with typo3/cms-core 12.3
    
    * Array syntax normalization
    
    * Update schema
    
    * Update to TYPO3v12.3.0, add platform.php 8.1
    
    * Endable Password Policy For Frontend Users
    
    * Dump DB after update TYPO3 to v12.3.0
    
    * Add extension icon
    
    * Remove internal by rector
    
    ---------
    
    Co-authored-by: Tymoteusz Motylewski <t.motylewski@gmail.com>

--- .ddev/import-if-empty.sh
+++ .ddev/import-if-empty.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Use a table that should exist in your database instead of "mytable"
 if ! mysql -e 'SELECT * FROM pages;' db > /dev/null; then
   echo 'loading db'
   # This assumes the db.sql.gz is in the root of your repository, but
--- packages/site_package/Configuration/TCA/Overrides/pages.php
+++ packages/site_package/Configuration/TCA/Overrides/pages.php
@@ -1,5 +1,5 @@
 <?php
-defined('TYPO3_MODE') || die();
+defined('TYPO3') || die();
 
 call_user_func(
     static function ($extensionKey) {
