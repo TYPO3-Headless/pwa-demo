b08172ecbffeff47c4e832a5c37132ed5b65294c
Adam <a.marcinkowski@macopedia.com>
2023-06-21 13:26:24 +0200

    Override pageData and initialData

---
+++ front/pages/_.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <p>Overrided page component</p>
+    <t3-dynamic
+      :data="t3page.content"
+      :type="t3page.appearance.backendLayout"
+      layout
+    />
+    <PageInformation/>
+  </div>
+</template>
+<script>
+import pageDefault from "~typo3/pages/_.vue";
+export default {
+  extends: pageDefault,
+};
+</script>
