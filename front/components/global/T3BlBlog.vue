06e225cd5b50cc8ce9f311f47f3f1279e05a1149
Adam <a.marcinkowski@macopedia.com>
2024-02-28 16:38:33 +0100

    Use frontend and backend layouts

---
+++ front/components/global/T3BlBlog.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <div class="columns-2">
+            <T3Renderer
+                v-if="content?.colPos0"
+                :content="content.colPos0"
+            />
+            <T3Renderer
+                v-if="content?.colPos1"
+                :content="content.colPos1"
+            />
+        </div>
+        <div>
+            <T3Renderer
+                v-if="content?.colPos2"
+                :content="content.colPos2"
+            />
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { T3BackendLayout } from '@t3headless/nuxt-typo3'
+defineProps<T3BackendLayout>()
+</script>
--- front/pages/[...slug].vue
+++ front/pages/[...slug].vue
@@ -1,13 +1,15 @@
 <template>
-
-    custom field value: {{  pageData?.customField }}
-    <T3BackendLayout
-        v-if="pageData?.content"
-        :name="backendLayout"
-        :content="pageData.content"
-    />
+    <NuxtLayout :name="frontendLayout ||  'default'">
+        <T3BackendLayout
+            v-if="pageData?.content"
+            :name="backendLayout"
+            :content="pageData.content" />
+    </NuxtLayout>
 </template>
 
 <script setup lang="ts">
-const { pageData, backendLayout } = await useT3Page()
+const { pageData, backendLayout, frontendLayout } = await useT3Page()
+definePageMeta({
+    layout: false
+})
 </script>
