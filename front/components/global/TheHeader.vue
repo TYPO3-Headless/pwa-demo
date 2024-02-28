f35759ea2c4f36b422018590e3e2d4f7a14fb21f
Adam <a.marcinkowski@macopedia.com>
2024-02-28 16:23:30 +0100

    Override page data / initial data

---
+++ front/components/global/TheHeader.vue
@@ -0,0 +1,22 @@
+<template>
+    <header class="container mx-auto my-3">
+        <nav v-if="initialData?.customMenu?.length && initialData.customMenu[0].children">
+            <ul class="flex justify-center">
+                <li
+                    v-for="{ link, title } in initialData.customMenu[0].children"
+                    :key="link"
+                    class="mx-1"
+                >
+                    <T3Link :link="link">
+                        {{ title }}
+                    </T3Link>
+                </li>
+            </ul>
+        </nav>
+    </header>
+    <Languages/>
+</template>
+
+<script setup lang="ts">
+const { initialData } = useT3Api()
+</script>
