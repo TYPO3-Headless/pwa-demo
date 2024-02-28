6e5bf9f7755a129bda662c94854fdb4e9f68bb3c
Adam <a.marcinkowski@macopedia.com>
2024-02-28 16:23:30 +0100

    Override page data / initial data

---
+++ front/components/global/Languages.vue
@@ -0,0 +1,11 @@
+<template>
+    <ul>
+        <li v-for="(item, key) in initialData.i18n" :key="key">
+            {{ item.title}}
+        </li>
+    </ul>
+</template>
+
+<script setup lang="ts">
+const { initialData } = await useT3Api()
+</script>
