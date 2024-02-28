8e0db0d536901cbc25df1c8c4dc57c915cba664a
Adam <a.marcinkowski@macopedia.com>
2024-02-28 16:12:24 +0100

    Override and create new Content Elements

---
+++ front/components/global/T3CeText.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="bg-primary text-white">
+        newField value: {{ newField }}
+        <T3HtmlParser v-if="bodytext" :content="bodytext"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+import type { T3CeTextProps } from '@t3headless/nuxt-typo3';
+
+interface MyT3CeTextProps extends T3CeTextProps {
+    newField: string
+}
+
+defineProps<MyT3CeTextProps>()
+</script>
