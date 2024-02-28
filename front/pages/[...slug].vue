9896afd926238c47bbe3617268c2510e424608c1
Adam <a.marcinkowski@macopedia.com>
2024-02-28 16:23:30 +0100

    Override page data / initial data

---
+++ front/pages/[...slug].vue
@@ -0,0 +1,13 @@
+<template>
+
+    custom field value: {{  pageData?.customField }}
+    <T3BackendLayout
+        v-if="pageData?.content"
+        :name="backendLayout"
+        :content="pageData.content"
+    />
+</template>
+
+<script setup lang="ts">
+const { pageData, backendLayout } = await useT3Page()
+</script>
